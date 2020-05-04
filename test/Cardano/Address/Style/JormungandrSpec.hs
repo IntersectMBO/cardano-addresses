1e956eb13c933c65e8a19f730f99c95797c12f61
Pawel Jakubas <pawel.jakubas@iohk.io>
2020-05-04 14:33:23 +0200

    add tests for derivation

---
+++ test/Cardano/Address/Style/JormungandrSpec.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Cardano.Address.Style.JormungandrSpec
+    ( spec
+    ) where
+
+import Prelude
+
+import Cardano.Address.Derivation
+    ( AccountingStyle (..)
+    , Depth (..)
+    , DerivationType (..)
+    , GenMasterKey (..)
+    , HardDerivation (..)
+    , Index
+    , SoftDerivation (..)
+    , XPrv
+    , toXPub
+    )
+import Cardano.Address.Style.Jormungandr
+    ( Jormungandr (..) )
+import Cardano.Mnemonic
+    ( SomeMnemonic )
+import Data.ByteArray
+    ( ByteArrayAccess, ScrubbedBytes )
+import Test.Arbitrary
+    ()
+import Test.Hspec
+    ( Spec, describe, it )
+import Test.QuickCheck
+    ( Arbitrary (..)
+    , Property
+    , choose
+    , expectFailure
+    , property
+    , vector
+    , (===)
+    , (==>)
+    )
+
+import qualified Data.ByteArray as BA
+import qualified Data.ByteString as BS
+
+spec :: Spec
+spec = do
+    describe "BIP-0044 Derivation Properties" $ do
+        it "deriveAccountPrivateKey works for various indexes" $
+            property prop_accountKeyDerivation
+        it "N(CKDpriv((kpar, cpar), i)) === CKDpub(N(kpar, cpar), i)" $
+            property prop_publicChildKeyDerivation
+
+    describe "Bounded / Enum relationship" $ do
+        it "Calling toEnum for invalid value gives a runtime err (AccountingStyle)"
+            (property prop_toEnumAccountingStyle)
+
+    describe "Enum Roundtrip" $ do
+        it "AccountingStyle" (property prop_roundtripEnumAccountingStyle)
+
+{-------------------------------------------------------------------------------
+                                 Properties
+-------------------------------------------------------------------------------}
+
+prop_publicChildKeyDerivation
+    :: (SomeMnemonic, SndFactor)
+    -> AccountingStyle
+    -> Index 'Soft 'AddressK
+    -> Property
+prop_publicChildKeyDerivation (mw, (SndFactor sndFactor)) cc ix =
+    addrXPub1 === addrXPub2
+  where
+    rootXPrv = genMasterKeyFromMnemonic mw sndFactor :: Jormungandr 'RootK XPrv
+    accXPrv  = deriveAccountPrivateKey rootXPrv minBound
+    addrXPub1 = toXPub <$> deriveAddressPrivateKey accXPrv cc ix
+    addrXPub2 = deriveAddressPublicKey (toXPub <$> accXPrv) cc ix
+
+prop_accountKeyDerivation
+    :: (SomeMnemonic, SndFactor)
+    -> Index 'Hardened 'AccountK
+    -> Property
+prop_accountKeyDerivation (mw, (SndFactor sndFactor)) ix =
+    accXPrv `seq` property ()
+  where
+    rootXPrv = genMasterKeyFromMnemonic mw sndFactor :: Jormungandr 'RootK XPrv
+    accXPrv = deriveAccountPrivateKey rootXPrv ix
+
+prop_toEnumAccountingStyle :: Int -> Property
+prop_toEnumAccountingStyle n =
+    n > fromEnum UTxOInternal ==> expectFailure $ property $
+        (toEnum n :: AccountingStyle) `seq` ()
+
+prop_roundtripEnumAccountingStyle :: AccountingStyle -> Property
+prop_roundtripEnumAccountingStyle ix =
+    (toEnum . fromEnum) ix === ix
+
+{-------------------------------------------------------------------------------
+                             Arbitrary Instances
+-------------------------------------------------------------------------------}
+
+newtype SndFactor = SndFactor ScrubbedBytes
+    deriving stock (Eq, Show)
+    deriving newtype (ByteArrayAccess)
+
+instance Arbitrary SndFactor  where
+    arbitrary = do
+        n <- choose (0, 64)
+        bytes <- BS.pack <$> vector n
+        return $ SndFactor $ BA.convert bytes
