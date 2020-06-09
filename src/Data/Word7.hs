a2e2e5e5da8c207c70457d322ad95b2ccc4ea48b
KtorZ <matthias.benkort@gmail.com>
2020-06-09 13:23:40 +0200

    implement 'getVariableLengthNat' in Data.Word7
    Enable pointer inspection in Shelley addresses.

--- src/Data/Word7.hs
+++ src/Data/Word7.hs
@@ -15,8 +15,9 @@ module Data.Word7
     , toWord7s
     , toNatural
 
-      -- * Encode
+      -- * Encode / Decode
     , putVariableLengthNat
+    , getVariableLengthNat
 
       -- * Helpers
     , limit
@@ -24,10 +25,14 @@ module Data.Word7
 
 import Prelude
 
+import Data.Binary.Get
+    ( Get, getWord8 )
 import Data.Binary.Put
     ( Put, putWord8 )
 import Data.Bits
-    ( shiftR, (.&.), (.|.) )
+    ( shiftL, shiftR, (.&.), (.|.) )
+import Data.List
+    ( foldl' )
 import Data.Word
     ( Word8 )
 import Numeric.Natural
@@ -63,10 +68,15 @@ toWord8 (Word7 x) = x
 -- > [Word7 1,Word7 21,Word7 27]
 toWord7s :: Natural -> [Word7]
 toWord7s = reverse . go
-    where
-        go n
-            | n <= 0x7F = [Word7 . fromIntegral $ n]
-            | otherwise = (toWord7 . fromIntegral) n : go (shiftR n 7)
+  where
+    go n
+        | n <= 0x7F = [Word7 . fromIntegral $ n]
+        | otherwise = (toWord7 . fromIntegral) n : go (shiftR n 7)
+
+word7sToNat :: [Word7] -> Natural
+word7sToNat = foldl' f 0
+  where
+    f n (Word7 r) = shiftL n 7 .|. (fromIntegral r)
 
 toNatural :: [Word7] -> Natural
 toNatural =
@@ -84,8 +94,18 @@ limit pow = 2 ^ pow - 1
 --
 putVariableLengthNat :: Natural -> Put
 putVariableLengthNat = putWord7s . toWord7s
+  where
+    putWord7s :: [Word7] -> Put
+    putWord7s [] = pure ()
+    putWord7s [Word7 x] = putWord8 x
+    putWord7s (Word7 x : xs) = putWord8 (x .|. 0x80) >> putWord7s xs
 
-putWord7s :: [Word7] -> Put
-putWord7s [] = pure ()
-putWord7s [Word7 x] = putWord8 x
-putWord7s (Word7 x : xs) = putWord8 (x .|. 0x80) >> putWord7s xs
+getVariableLengthNat :: Get Natural
+getVariableLengthNat = word7sToNat <$> getWord7s
+  where
+    getWord7s :: Get [Word7]
+    getWord7s = do
+      next <- getWord8
+      case next .&. 0x80 of
+        0x80 -> (:) (toWord7 next) <$> getWord7s
+        _ -> pure [Word7 next]
