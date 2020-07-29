16ac766751f55fb355c7dbb4db01c10f5ec91bdb
KtorZ <matthias.benkort@gmail.com>
2020-07-29 08:46:27 +0200

    run command-line tests with forced UTF-8 encoding
      Mainly, to cope with Windows and UTF-8 characters appearing in test descriptions.

--- command-line/test/Main.hs
+++ command-line/test/Main.hs
@@ -1,1 +1,13 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
+
+import Prelude
+
+import Command
+    ( withUtf8Encoding )
+import Test.Hspec.Runner
+    ( defaultConfig, hspecWith )
+
+import qualified AutoDiscover
+
+main :: IO ()
+main = withUtf8Encoding $ hspecWith defaultConfig AutoDiscover.spec
