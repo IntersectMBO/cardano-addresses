b45cd78eca8bffcd4f79dc91dc3c01703a8a8cc1
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-09-21 01:44:56 +1200

    Bump haskell.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.follows = "haskellNix/nixpkgs-2305";
     haskellNix = {
-      url = "github:input-output-hk/haskell.nix/angerman/fix-win-thdll";
+      url = "github:input-output-hk/haskell.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     CHaP = {
