80d696adcd82a518754861838d3d609b14a880db
Moritz Angermann <moritz.angermann@gmail.com>
2024-06-12 14:21:20 +0800

    Fix windows utf-8 issues (#256)
    * Fix windows utf-8 issues
    
    This ensures we force the right codepages on windows to support utf8 characters in the Haskell source for the tests.

--- command-line/test/Main.hs
+++ command-line/test/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module Main where
 
 import Prelude
@@ -11,7 +12,31 @@ import Test.Hspec.Runner
 
 import qualified AutoDiscover
 
+-- This is effectively from the now defunct foundation/basement from
+--  https://github.com/haskell-foundation/foundation/blob/5e28e3ea1e2fe9a98c157df463bd32d3f92e7f80/basement/Basement/Terminal.hs#L14-L26
+#ifdef mingw32_HOST_OS
+import System.IO
+    ( hPutStrLn, hSetEncoding, stderr, stdin, stdout, utf8 )
+import System.Win32.Console
+    ( getConsoleCP, getConsoleOutputCP, setConsoleCP, setConsoleOutputCP )
+#endif
+
+#ifdef mingw32_HOST_OS
+initialize :: IO ()
+initialize = do
+    query getConsoleOutputCP (\e -> setConsoleOutputCP e >> hSetEncoding stdout utf8 >> hSetEncoding stderr utf8) utf8Code
+    query getConsoleCP (\e -> setConsoleCP e >> hSetEncoding stdin utf8) utf8Code
+  where
+    utf8Code = 65001
+    query get set expected = do
+        v <- get
+        if v == expected then pure () else set expected
+#endif
+
 main :: IO ()
 main = do
     ghcjsBuildSupport
+#ifdef mingw32_HOST_OS
+    initialize
+#endif
     withUtf8 $ hspecWith defaultConfig AutoDiscover.spec
