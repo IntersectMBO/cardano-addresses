2bbb2de176d4f196d5d3ef3343bb2ce3391f3104
Pawel Jakubas <pawel.jakubas@iohk.io>
2024-07-24 16:17:20 +0200

    more PrivateSpec 1

--- command-line/test/Command/Key/PrivateSpec.hs
+++ command-line/test/Command/Key/PrivateSpec.hs
@@ -7,13 +7,14 @@ module Command.Key.PrivateSpec
 import Prelude
 
 import Test.Hspec
-    ( Spec, SpecWith, it, shouldBe, shouldContain )
+    ( Spec, SpecWith, it, shouldBe, shouldContain, shouldStartWith )
 import Test.Utils
     ( cli, describeCmd )
 
 spec :: Spec
 spec = describeCmd [ "key", "private" ] $ do
     specKeyNotPrivate
+    specKeyPrivate "icarus" "addr_sk" "1852H/1815H/0H/0/0" "--signing-key"
 
 specKeyNotPrivate :: SpecWith ()
 specKeyNotPrivate = it "fail if key isn't private" $ do
@@ -24,3 +25,11 @@ specKeyNotPrivate = it "fail if key isn't private" $ do
 
     out `shouldBe` ""
     err `shouldContain` "Invalid human-readable prefix."
+
+specKeyPrivate :: String -> String -> String -> String -> SpecWith ()
+specKeyPrivate style hrp path cc = it "succeeds if key is private" $ do
+    out <- cli [ "recovery-phrase", "generate" ] ""
+       >>= cli [ "key", "from-recovery-phrase", style ]
+       >>= cli [ "key", "child", path ]
+       >>= cli [ "key", "private", cc ]
+    out `shouldStartWith` hrp
