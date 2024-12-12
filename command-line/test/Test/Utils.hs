a05902fd795ad25b9b25f9c230456fd1ddd29bb0
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-12-12 11:07:40 +0100

    Remove dependency on hjsonschema
    This is needed for #285

--- command-line/test/Test/Utils.hs
+++ command-line/test/Test/Utils.hs
@@ -3,19 +3,21 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Test.Utils
-    ( cli
-    , describeCmd
-    , validateJSON
-    , SchemaRef
-    ) where
+module Test.Utils (
+    cli,
+    describeCmd,
+    validateJSON,
+    SchemaRef,
+) where
 
 import Prelude
 
-import Data.String
-    ( IsString )
-import Data.Text
-    ( Text )
+import Data.String (
+    IsString,
+ )
+import Data.Text (
+    Text,
+ )
 #ifdef HJSONSCHEMA
 import JSONSchema.Draft4
     ( Schema (..)
@@ -25,27 +27,34 @@ import JSONSchema.Draft4
     , referencesViaFilesystem
     )
 #endif
-import System.Environment
-    ( lookupEnv )
-import System.Process
-    ( readProcess, readProcessWithExitCode )
-import Test.Hspec
-    ( Spec, SpecWith, describe, runIO )
+import System.Environment (
+    lookupEnv,
+ )
+import System.Process (
+    readProcess,
+    readProcessWithExitCode,
+ )
+import Test.Hspec (
+    Spec,
+    SpecWith,
+    describe,
+    runIO,
+ )
 
 import qualified Data.Aeson as Json
-
 
 --
 -- cli
 --
 
 class CommandLine output where
-    cli :: [String]
-           -- ^ arguments
-        -> String
-            -- ^ stdin
-        -> IO output
-            -- ^ output, either stdout or (stdout, stderr)
+    cli ::
+        -- | arguments
+        [String] ->
+        -- | stdin
+        String ->
+        -- | output, either stdout or (stdout, stderr)
+        IO output
 
 instance CommandLine String where
     cli args input = do
@@ -57,32 +66,33 @@ instance CommandLine (String, String) where
         (exe', args') <- getWrappedCLI args
         dropFirst <$> readProcessWithExitCode exe' args' input
       where
-        dropFirst (_,b,c) = (b, c)
+        dropFirst (_, b, c) = (b, c)
 
 exe :: String
 exe = "cardano-address"
 
--- | Return the exe name and args for a CLI invocation.
--- For ghcjs, the cardano-address CLI must be executed under nodejs.
+{- | Return the exe name and args for a CLI invocation.
+For ghcjs, the cardano-address CLI must be executed under nodejs.
+-}
 getWrappedCLI :: [String] -> IO (FilePath, [String])
 getWrappedCLI args = maybe (exe, args) wrap <$> lookupJsExe
   where
     lookupJsExe = fmap allJs <$> lookupEnv "CARDANO_ADDRESSES_CLI"
     allJs = (<> ("/" <> exe <> ".jsexe/all.js"))
-    wrap jsExe = ("node", (jsExe:args))
+    wrap jsExe = ("node", (jsExe : args))
 
 --
 -- describeCmd
 --
 
--- | Wrap HSpec 'describe' into a friendly command description. So that, we get
--- a very satisfying result visually from running the tests, and can inspect
--- what each command help text looks like.
+{- | Wrap HSpec 'describe' into a friendly command description. So that, we get
+a very satisfying result visually from running the tests, and can inspect
+what each command help text looks like.
+-}
 describeCmd :: [String] -> SpecWith () -> Spec
 describeCmd cmd spec = do
     title <- runIO $ cli (cmd ++ ["--help"]) ""
     describe title spec
-
 
 --
 -- JSON Schema Validation
@@ -90,7 +100,8 @@ describeCmd cmd spec = do
 
 newtype SchemaRef = SchemaRef
     { getSchemaRef :: Text
-    } deriving (Show, IsString)
+    }
+    deriving (Show, IsString)
 
 validateJSON :: SchemaRef -> Json.Value -> IO [String]
 #ifdef HJSONSCHEMA
