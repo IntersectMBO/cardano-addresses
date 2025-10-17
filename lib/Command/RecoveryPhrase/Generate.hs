4b1a274b0717bcb1e494aa91735345d767fc6929
Pawel Jakubas <pawel.jakubas@iohk.io>
2025-10-17 16:20:45 +0200

    use MnemonicLanguage in Generate command

--- lib/Command/RecoveryPhrase/Generate.hs
+++ lib/Command/RecoveryPhrase/Generate.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeApplications #-}
 
@@ -22,6 +23,8 @@ import Cardano.Mnemonic
     ( english, entropyToMnemonic, genEntropy, mnemonicToText )
 import Options.Applicative
     ( CommandFields, Mod, command, helper, info, progDesc )
+import Options.Applicative.MnemonicLanguage
+    ( MnemonicLanguage (..), mnemonicLanguageOpt )
 import Options.Applicative.MnemonicSize
     ( MnemonicSize (..), mnemonicSizeOpt )
 
@@ -30,20 +33,22 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 
 
-newtype Cmd = Generate
+data Cmd = Generate
     { size :: MnemonicSize
+    , language :: MnemonicLanguage
     } deriving (Show)
 
 mod :: (Cmd -> parent) -> Mod CommandFields parent
 mod liftCmd = command "generate" $
     info (helper <*> fmap liftCmd parser) $ mempty
-        <> progDesc "Generate an English recovery phrase"
+        <> progDesc "Generate a recovery phrase for a specified mnemonic and language. "
   where
     parser = Generate
         <$> mnemonicSizeOpt
+        <*> mnemonicLanguageOpt
 
 run :: Cmd -> IO ()
-run Generate{size} = do
+run Generate{size,language} = do
     m <- case size of
         MS_9  -> mnemonicToText @9  . entropyToMnemonic <$> genEntropy
         MS_12 -> mnemonicToText @12 . entropyToMnemonic <$> genEntropy
@@ -51,4 +56,7 @@ run Generate{size} = do
         MS_18 -> mnemonicToText @18 . entropyToMnemonic <$> genEntropy
         MS_21 -> mnemonicToText @21 . entropyToMnemonic <$> genEntropy
         MS_24 -> mnemonicToText @24 . entropyToMnemonic <$> genEntropy
-    B8.putStrLn $ T.encodeUtf8 $ T.unwords $ m english
+    B8.putStrLn $ T.encodeUtf8 $ T.unwords $ m $ dictionaryFromLanguage language
+  where
+    dictionaryFromLanguage = \case
+        English -> english
