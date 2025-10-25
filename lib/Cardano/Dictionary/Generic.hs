389d83ddf101f6f76b3ce74631cbe01e35fe6c1c
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-10-25 20:37:04 +0200

    introduce Cardano.Dictionary.Generic

---
+++ lib/Cardano/Dictionary/Generic.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+{-# OPTIONS_HADDOCK hide #-}
+
+-- |
+-- Copyright: 2025 Intersect
+-- License: Apache-2.0
+
+module Cardano.Dictionary.Generic
+    (
+    -- * Helper
+     toDictionary
+    ) where
+
+import Basement.Imports
+    ( Bounded(..)
+    , Either(..)
+    , Maybe(..)
+    , (.)
+    , ($)
+    , error
+    , flip
+    , fromList
+    )
+import Basement.Sized.Vect (Vect (..), index, toVect)
+import Basement.String (String)
+import Crypto.Encoding.BIP39.Dictionary
+    ( Dictionary (..)
+    , DictionaryError (..)
+    , WordIndex
+    , unWordIndex
+    )
+import Data.Maybe (fromMaybe)
+
+import qualified Data.List as L
+
+
+toDictionary :: [String] -> Dictionary
+toDictionary wordList = Dictionary
+    { dictionaryWordToIndex =
+          \word -> case L.lookup word wordsWithIxs of
+                     Just x  -> Right x
+                     Nothing -> Left $ ErrInvalidDictionaryWord word
+    , dictionaryTestWord = flip L.elem wordList
+    , dictionaryIndexToWord = index words . unWordIndex
+    , dictionaryWordSeparator = " "
+    }
+  where
+    wordsWithIxs :: [(String, WordIndex)]
+    wordsWithIxs = L.zip wordList [minBound..maxBound]
+
+    words :: Vect 2048 String
+    words = fromMaybe (error "invalid vector length") $ toVect $ fromList wordList
