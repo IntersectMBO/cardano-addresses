fe3acb4367edb89a3da1befeba2e18008cb1f439
Pawel Jakubas <jakubas.pawel@gmail.com>
2026-02-23 14:05:59 +0100

    fix: use CPP for git revision to enable cross-compilation
    - Replace runtime git invocation with CPP macro
    - Default to 'unknown' if GITREV not provided
    - Fixes cross-compilation (Linux -> Windows via iserv-proxy)

--- lib/System/Git/TH.hs
+++ lib/System/Git/TH.hs
@@ -1,41 +1,14 @@
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-{-# OPTIONS_HADDOCK hide #-}
+module System.Git.TH (gitRevParseHEAD) where
 
--- |
--- Copyright: 2020 Input Output (Hong Kong) Ltd., 2021-2022 Input Output Global Inc. (IOG), 2023-2025 Intersect
--- License: Apache-2.0
-
-module System.Git.TH
-    ( gitRevParseHEAD
-    ) where
-
-import Prelude
-
-import Control.Exception
-    ( SomeException, try )
-import Language.Haskell.TH
-    ( Exp (..), Lit (..), Q, runIO )
-import System.Environment
-    ( lookupEnv )
-import System.Exit
-    ( ExitCode (..) )
-import System.Process
-    ( readProcessWithExitCode )
+import Control.Monad (return)
+import Language.Haskell.TH (Exp (..), Lit (..), Q)
 
 gitRevParseHEAD :: Q Exp
-gitRevParseHEAD =
-    LitE . StringL <$> runIO findGitRev
-  where
-    findGitRev :: IO String
-    findGitRev = do
-        envRev <- lookupEnv "GITREV"
-        maybe runGitRevParse pure envRev
-
-    runGitRevParse :: IO String
-    runGitRevParse = do
-        result <- try @SomeException $
-            readProcessWithExitCode "git" ["rev-parse", "--verify", "HEAD"] ""
-        case result of
-            Right (ExitSuccess, revision, _) -> pure revision
-            _ -> pure "unknown revision"
+#ifdef GITREV
+gitRevParseHEAD = return (LitE (StringL GITREV))
+#else
+gitRevParseHEAD = return (LitE (StringL "unknown"))
+#endif
