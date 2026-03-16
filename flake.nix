94c244407e553a9f1c1f0c69fc71f35502dc4c68
Pawel Jakubas <jakubas.pawel@gmail.com>
2026-03-16 12:42:53 +0100

    correct flake.nix

--- flake.nix
+++ flake.nix
@@ -3,48 +3,38 @@
 
   inputs = {
     nixpkgs.follows = "devx/nixpkgs";
-    hostNixpkgs.follows = "nixpkgs";
     devx.url = "github:input-output-hk/devx";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, devx, hostNixpkgs, ... }:
+  outputs = { self, nixpkgs, flake-utils, devx, ... }:
     let
       inherit (nixpkgs) lib;
       inherit (flake-utils.lib) eachSystem mkApp;
       supportedSystems = import ./nix/supported-systems.nix;
-      defaultSystem = lib.head supportedSystems;
-      overlay = final: prev: { };
     in
     {
-      inherit overlay;
+      devShells = lib.genAttrs supportedSystems (system:
+        devx.outputs.devShells.${system}
+      );
     } // eachSystem supportedSystems
       (system:
         let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ devx.overlays.default overlay ];
-          };
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShells.default = devx.outputs.devShells.${system}.ghc96-iog;
 
-          flake = devx.outputs.legacyPackages.${system}.flake;
-
-          baseFlake = lib.recursiveUpdate flake {
-            hydraJobs.devShells = flake.devShells;
-
-            apps = {
-              repl = mkApp {
-                drv = pkgs.writeShellScriptBin "repl" ''
-                  confnix=$(mktemp)
-                  echo "builtins.getFlake (toString $(git rev-parse --show-toplevel))" >$confnix
-                  trap "rm $confnix" EXIT
-                  nix repl $confnix
-                '';
-              };
+          apps = {
+            repl = mkApp {
+              drv = pkgs.writeShellScriptBin "repl" ''
+                confnix=$(mktemp)
+                echo "builtins.getFlake (toString $(git rev-parse --show-toplevel))" >$confnix
+                trap "rm $confnix" EXIT
+                nix repl $confnix
+              '';
             };
           };
-
-          docker = if system == "x86_64-linux" then { } else { };
-        in
-        baseFlake // docker
+        }
       );
 }
