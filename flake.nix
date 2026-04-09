85471435bab38b54c257cf137a03aa9fc47efda8
paolino <paolo.veronelli@gmail.com>
2026-04-09 15:49:58 +0100

    nix: export WASM binary as flake output
    packages.wasm builds cardano-addresses-wasm.wasm via the FOD pattern:
    - Phase 1 (FOD): fetch Hackage deps with network access
    - Phase 2 (pure): build offline with ghc-wasm-meta toolchain
    
    Usage:
      nix build github:paolino/cardano-addresses/001-wasm-target#wasm
      ls result/cardano-addresses.wasm

--- flake.nix
+++ flake.nix
@@ -5,9 +5,14 @@
     nixpkgs.follows = "devx/nixpkgs";
     devx.url = "github:input-output-hk/devx";
     flake-utils.url = "github:numtide/flake-utils";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    ghc-wasm-meta = {
+      url = "gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org";
+      flake = true;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, devx, ... }:
+  outputs = { self, nixpkgs, flake-utils, devx, haskellNix, ghc-wasm-meta, ... }:
     let
       inherit (nixpkgs) lib;
       inherit (flake-utils.lib) eachSystem mkApp;
@@ -20,10 +25,31 @@
     } // eachSystem supportedSystems
       (system:
         let
-          pkgs = import nixpkgs { inherit system; };
+          pkgs = import nixpkgs {
+            inherit system;
+          };
         in
         {
           devShells.default = devx.outputs.devShells.${system}.ghc96-iog;
+
+          packages =
+            let
+              # Limit haskell.nix to the WASM packaging path only.
+              pkgsWasm = import nixpkgs {
+                inherit system;
+                overlays = [ haskellNix.overlay ];
+              };
+              wasmBuild = import ./nix/wasm.nix {
+                pkgs = pkgsWasm;
+                ghcWasmToolchain = ghc-wasm-meta.packages.${system}.all_9_12;
+                src = ./.;
+                dependenciesHash = "sha256-tn90dlUhluIKWvHcxm8S8nBp06ysqHY5cUgKiLjmwJc=";
+              };
+            in
+            {
+              wasm = wasmBuild.wasm;
+              wasm-deps = wasmBuild.deps;
+            };
 
           apps = {
             repl = mkApp {
